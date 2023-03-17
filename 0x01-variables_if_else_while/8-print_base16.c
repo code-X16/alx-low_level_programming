e6ed94585a5b440fa60233c909b1f58742f44649
Bakare <Bakare>
2023-03-17 13:19:25 -0700

    Added task

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, j;
+	for (i = 0; i < 10; i++)
+		putchar(i);
+	for (j = 'a'; j <= 'f'; j++)
+		putchar(j);
+	putchar('\n');
+
+	return (0);
+}
