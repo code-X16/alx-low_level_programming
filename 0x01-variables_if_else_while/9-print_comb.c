85f7a586e7838fc720d30d21a8eb7dc8279c99fd
Bakare <Bakare>
2023-03-17 14:14:21 -0700

    Added task

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,14 +8,17 @@
 
 int main(void)
 {
-	int i;
+	int i = '0';
 
-	for (i = '0'; i <= '9'; i++)
-		putchar(i);
-	if (i != '9')
+	while (i <= '9')
 	{
-		putchar(',');
-		putchar(' ');
+		putchar(i);
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
 	}
 	putchar('\n');
 
