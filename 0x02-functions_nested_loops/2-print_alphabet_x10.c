9441a964608f673225bc960bd3fab4c305d567c0
Bakare <Bakare>
2023-03-21 07:41:56 -0700

    Added task

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line.
+ *
+ * Return: NULL
+ */
+
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 1; i <= 10; i++)
+		for (j = 'a'; j <= 'z'; j++)
+			_putchar(j);
+	_putchar('\n');
+}
