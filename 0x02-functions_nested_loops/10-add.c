b04a1007cfff1148e48835a170b2a7bdb73e7811
Bakare <Bakare>
2023-03-21 15:16:14 -0700

    Added task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result.
+ * @a: first interger to be added.
+ * @b: second interger to be added.
+ *
+ * Return: result
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
