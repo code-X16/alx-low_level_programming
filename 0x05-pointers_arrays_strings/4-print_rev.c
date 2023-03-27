7641fdea358a46b8081f9743d3233f07ce5c977b
Bakare <Bakare>
2023-03-27 09:46:19 -0700

    Added task

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - function that prints a string, in reverse
+ * @s: string to print
+ *
+ * Return: 0
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	while (i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
