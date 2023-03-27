ce59292c2ab922f29078c37952c25a182bbca5dc
Bakare <Bakare>
2023-03-27 09:32:23 -0700

    Added task

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints a string
+ * @str: string to print
+ *
+ * Return: 0
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
